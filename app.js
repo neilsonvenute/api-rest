b721660e9f8057e5f2bde553fdf697d449a72bf9
Neilson Venute <neilsonvenute@gmail.com>
2022-04-09 21:44:49 -0300

    correção do cors

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ import userRoutes from './src/routes/userRoutes';
 dotenv.config();
 
 const whiteList = [
-  'http://localhost:3000',
+  'http://localhost:3001',
 ];
 
 const corsOtpions = {
   origin(origin, callback) {
     if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
       callback(new Error('Not allowed by CORSS'));
     }
   },
